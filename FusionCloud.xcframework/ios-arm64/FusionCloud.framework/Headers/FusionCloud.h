7fa6cc2360d3e8cb8f345b7221768e1f80e0ab41
Vanessa Agustin <vanessa_agustin@yahoo.com>
2023-04-03 17:15:49 +1000

    new build

---
+++ FusionCloud.xcframework/ios-arm64/FusionCloud.framework/Headers/FusionCloud.h
@@ -0,0 +1,18 @@
+//
+//  FusionCloud.h
+//  FusionCloud
+//
+//  Created by Loey Agdan on 5/9/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FusionCloud.
+FOUNDATION_EXPORT double FusionCloudVersionNumber;
+
+//! Project version string for FusionCloud.
+FOUNDATION_EXPORT const unsigned char FusionCloudVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FusionCloud/PublicHeader.h>
+
+
